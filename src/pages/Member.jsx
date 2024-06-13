70a979eaac8d656fab179ce754e77e5158c6ad68
youwoon <youwoon5520@gmail.com>
2024-06-13 16:55:15 +0900

    update:회원가입 api연동

--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Container, InputGroup, ToggleButton, Button } from '../components/MemberStyled';
+import { register } from '../lib/api/auth';
 
 function Member() {
     const [id, setId] = useState('');
@@ -8,17 +9,17 @@ function Member() {
     const [user, setUser] = useState('');
     const navigate = useNavigate();
 
-    const Login = (e) => {
+    const Login = async (e) => {
         e.preventDefault();
 
-        // ID 검증
+        // ID, 비밀번호, 닉네임 검증
         if (!id.trim()) {
-            alert('회원가입 공백');
+            alert('회원가입 아이디 공백');
             return;
         }
 
         if (!password.trim()) {
-            alert('비번 공백');
+            alert('비밀번호 공백');
             return;
         }
 
@@ -27,48 +28,50 @@ function Member() {
             return;
         }
 
-        alert('회원가입 성공');
-        navigate('/login');
+        const response = await register({ id: id, password: password, nickname: user });
+        if (response) {
+            confirm('가입 완료.');
+            navigate('/login');
+        }
     };
 
     return (
         <Container>
-            <form onSubmit={Login}>
-                <InputGroup>
-                    <label htmlFor="id">회원가입 아이디</label>
-                    <input
-                        type="text"
-                        placeholder="아이디 입력"
-                        value={id}
-                        onChange={(e) => setId(e.target.value)}
-                        minLength={4}
-                        maxLength={10}
-                    />
-                </InputGroup>
-                <InputGroup>
-                    <label htmlFor="password">비밀번호</label>
-                    <input
-                        type="password"
-                        placeholder="비밀번호 입력"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        minLength={4}
-                        maxLength={15}
-                    />
-                </InputGroup>
-                <InputGroup>
-                    <label htmlFor="user">닉네임</label>
-                    <input
-                        type="text"
-                        placeholder="닉네임 입력"
-                        value={user}
-                        onChange={(e) => setUser(e.target.value)}
-                        minLength={1}
-                        maxLength={10}
-                    />
-                </InputGroup>
-                <ToggleButton type="submit">회원가입</ToggleButton>
-            </form>
+            <InputGroup>
+                <label htmlFor="id">회원가입 아이디</label>
+                <input
+                    type="text"
+                    placeholder="아이디 입력"
+                    value={id}
+                    onChange={(e) => setId(e.target.value)}
+                    minLength={4}
+                    maxLength={10}
+                />
+            </InputGroup>
+            <InputGroup>
+                <label htmlFor="password">비밀번호</label>
+                <input
+                    type="password"
+                    placeholder="비밀번호 입력"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    minLength={4}
+                    maxLength={15}
+                />
+            </InputGroup>
+            <InputGroup>
+                <label htmlFor="user">닉네임</label>
+                <input
+                    type="text"
+                    placeholder="닉네임 입력"
+                    value={user}
+                    onChange={(e) => setUser(e.target.value)}
+                    minLength={1}
+                    maxLength={10}
+                />
+            </InputGroup>
+            <ToggleButton onClick={Login}>회원가입</ToggleButton>
+
             <Button
                 onClick={() => {
                     navigate('/login');
