5af692de3f5f37302b68c451a311490a28fcb677
youwoon <youwoon5520@gmail.com>
2024-06-13 19:45:15 +0900

    update: 새로고침 해도 id 유지

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,23 @@ import Member from './pages/Member';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { getUserInfo } from './lib/api/auth';
 
 function App() {
     const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        getUserInfo().then((res) => {
+            if (res) {
+                setUser({
+                    userId: res.id,
+                    nickname: res.nickname,
+                    avatar: res.avatar,
+                });
+            }
+        });
+    }, []);
 
     console.log('현재:', user);
     return (
--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -29,3 +29,20 @@ export const login = async ({ id, password }) => {
         alert(error?.response?.data?.message);
     }
 };
+
+export const getUserInfo = async () => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+        try {
+            const response = await axios.get(AUTH_API_HOST + '/user', {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+            return response.data;
+        } catch (error) {
+            console.log(error?.response?.data?.message);
+            alert(error?.response?.data?.message);
+        }
+    }
+};
